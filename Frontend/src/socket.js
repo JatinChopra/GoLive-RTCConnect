9b4b46a6aa6057fcb094ae53d4b87aa90baa4799
JatinChopra <jatinchopra2053@gmail.com>
2023-08-21 17:02:56 +0530

    fix: url change

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
-// const URL = "https://golive-rtcconnect.onrender.com/";
-const URL = "http://localhost:5000";
+const URL = "https://golive-rtcconnect.onrender.com/";
+// const URL = "http://localhost:5000";
 
 export const socket = io(URL, {
   autoConnect: false,
